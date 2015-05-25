9501ce5d4da9cd69acf7ecf901c5801c87a4c86e
jules <jul.mue@hotmail.de>
2015-05-25 23:54:45 +0200

    pretty printing

--- parser.ml
+++ parser.ml
@@ -1,5 +1,14 @@
+open Format
+(* ------------------------------------------------------------------------------------------------ *)
 (* helper list functions																			*)
 
+let length l =
+	let rec len k l =
+		match l with
+		| [] -> k
+		| _::t -> len (k+1) t
+	in len 0 l
+		
 let hd l =
 	match l with 
 	| h::t -> h
@@ -9,11 +18,6 @@ let tl l =
 	match l with
 	| h::t -> t
 	| _ -> failwith "tl"	(* why not []? *)
-
-let rec forall p l =
-	match l with
-	| [] -> true
-	| h::t -> p(h) && forall p t
 
 let explode s =
 	let rec exap n l =
@@ -26,10 +30,23 @@ let rec mem x l =
 	| [] -> false
 	| (h::t) -> Pervasives.compare x h = 0 || mem x t
 
+let rec forall p l =
+	match l with
+	| [] -> true
+	| h::t -> p(h) && forall p t
+
 let rec exists p l =
 	match l with
 	| [] -> false
 	| h::t -> p(h) || exists p t
+
+let rec do_list f l =
+	match l with
+	| [] -> ()
+	| x::t -> f x; do_list f t
+
+let rec el n l =
+	if n = 0 then hd l else el (n - 1) (tl l)
 
 (* ------------------------------------------------------------------------------------------------ *)
 (* ------------------------------------------------------------------------------------------------ *)
@@ -48,7 +65,6 @@ type ('a)formula =
 	| Iff of ('a)formula * ('a)formula
 	| Forall of string * ('a)formula
 	| Exists of string * ('a)formula
-
 
 (* ------------------------------------------------------------------------------------------------ *)
 (* prop for propositions *)
@@ -278,8 +294,111 @@ let parse_atom vs inp =
 let parse = make_parser
 	(parse_formula (parse_infix_atom,parse_atom) [])
 
+(* test: parse "sqrt(1-cos^2(x+y))" *)
 
 (* let default_parser = parse
 	let secondary_parser = parset
 *)
 
+(* ------------------------------------------------------------------------------------------------ *)
+(* pretty printing *)
+(* outputs directly to stdout using the "Format" library facilites									*)
+
+let bracket p n f x y =
+	(if p then print_string "(" else ());
+	open_box n;
+	f x y;
+	close_box();
+	(if p then print_string ")" else ())
+
+
+(* convention: opmitt the quantifiers symbol with repeated quantifiers								*)
+let rec strip_quant fm =
+	match fm with
+	| Forall(x,(Forall(y,p) as yp)) | Exists(x,(Exists(y,p) as yp)) -> 
+		let (xs,q) = strip_quant yp 
+		in (x::xs,q)
+	| Forall(x,p) | Exists(x,p) -> ([x],p)
+	| _ -> ([],fm);;
+
+
+(* printing is parameterized by a function to print atoms (pfn)										*)
+let print_formula pfn =
+	let rec print_formula pr fm =
+		match fm with
+		| False -> print_string "false"
+		| True -> print_string "true"
+		| Atom(pargs) -> pfn pr pargs
+		| Not(p) -> bracket (pr > 10) 1 (print_prefix 10) "~" p
+		| And(p,q) -> bracket (pr > 8) 0 (print_infix 8 "/\\") p q
+		| Or(p,q) -> bracket (pr > 6) 0 (print_infix 6 "\\/") p q
+		| Imp(p,q) -> bracket (pr > 4) 0 (print_infix 4 "==>") p q
+		| Iff(p,q) -> bracket (pr > 2) 0 (print_infix 2 "<=>") p q
+		| Forall(x,p) -> bracket (pr > 0) 2 print_qnt "forall" (strip_quant fm)
+		| Exists(x,p) -> bracket (pr > 0) 2 print_qnt "exists" (strip_quant fm)
+	and print_qnt qname (bvs,bod) =
+		print_string qname;
+		do_list (fun v -> print_string " "; print_string v) bvs;
+		print_string "."; print_space(); open_box 0;
+		print_formula 0 bod;
+		close_box()
+	and print_prefix newpr sym p =
+		print_string sym; print_formula (newpr+1) p
+	and print_infix newpr sym p q =
+		print_formula (newpr+1) p;
+		print_string(" "^sym); print_space();
+		print_formula newpr q in
+	print_formula 0	
+
+let print_qformula pfn fm =
+	open_box 0;
+	print_string "<<";
+	open_box 0;
+	print_formula pfn fm; close_box();
+	print_string ">>";
+	close_box()
+
+let rec print_term prec fm =
+	match fm with 
+	| Var x -> print_string x
+	| Fn("^",[tm1;tm2]) -> print_infix_term true prec 24 "^" tm1 tm2
+	| Fn("/",[tm1;tm2]) -> print_infix_term true prec 22 " /" tm1 tm2
+	| Fn("*",[tm1;tm2]) -> print_infix_term false prec 20 " *" tm1 tm2
+	| Fn("-",[tm1;tm2]) -> print_infix_term true prec 18 " -" tm1 tm2
+	| Fn("+",[tm1;tm2]) -> print_infix_term false prec 16 " +" tm1 tm2
+	| Fn("::",[tm1;tm2]) -> print_infix_term false prec 14 "::" tm1 tm2
+	| Fn(f,args) -> print_fargs f args
+and print_fargs f args =
+	print_string f;
+	if args = [] then () else
+	( print_string "(";
+		open_box 0;
+		print_term 0 (hd args); print_break 0 0;
+		do_list (fun t -> print_string ","; print_break 0 0; print_term 0 t)
+				(tl args);
+		close_box();
+		print_string ")")
+and print_infix_term isleft oldprec newprec sym p q =
+	if oldprec > newprec then (print_string "("; open_box 0) else ();
+	print_term (if isleft then newprec else newprec+1) p;
+	print_string sym;
+	print_break (if String.sub sym 0 1 = "" then 1 else 0)0;
+	print_term (if isleft then newprec+1 else newprec) q;
+	if oldprec > newprec then (close_box(); print_string ")") else ()
+
+(* printing of terms *)
+let printert tm =
+	open_box 0; print_string "<<|";
+	open_box 0; print_term 0 tm; close_box();
+	print_string "|>>"; close_box()
+
+let print_atom prec (R(p,args)) =
+	if mem p ["="; "<"; "<="; ">"; ">="] && length args = 2
+	then print_infix_term false 12 12 (" "^p) (el 0 args) (el 1 args)
+	else print_fargs p args
+
+
+let print_fol_formula = print_qformula print_atom
+
+let print_propvar prec p = print_string(pname p)
+let print_prop_formula = print_qformula print_propvar
